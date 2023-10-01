ebd7d39b5363f02eb501847239f51402a64e0e1a
Baris Ceviz <baris@ceviz.dev>
2023-10-01 02:15:08 +0200

    feat: implement port forwarding endpoints

--- modems/sagemcom/port_forwarding.go
+++ modems/sagemcom/port_forwarding.go
@@ -9,7 +9,7 @@ import (
 type PortForwardingRule struct {
 	ID   int `json:"id"`
 	Rule struct {
-		Enabled           bool   `json:"enabled"`
+		Enabled           bool   `json:"enable"`
 		ExternalStartPort int    `json:"externalStartPort"`
 		ExternalEndPort   int    `json:"externalEndPort"`
 		Protocol          string `json:"protocol"`
@@ -17,7 +17,14 @@ type PortForwardingRule struct {
 		LocalEndPort      int    `json:"localEndPort"`
 		LocalAddress      string `json:"localAddress"`
 		ReadOnly          bool   `json:"readOnly"`
-	}
+	} `json:"rule"`
+}
+
+type CreatePortForwardingResponse struct {
+	Created struct {
+		ID  int    `json:"id"`
+		URI string `json:"uri"`
+	} `json:"created"`
 }
 
 type PortForwardingResponse struct {
@@ -51,4 +58,43 @@ func (c *PortForwardingClient) GetPortForwardings() (*PortForwardingResponse, er
 	}
 
 	return &response, nil
+}
+
+func (c *PortForwardingClient) CreatePortForwarding(req PortForwardingRule) (*CreatePortForwardingResponse, error) {
+	resp, err := c.httpClient.POST("network/portforwarding", nil, req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	var response CreatePortForwardingResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &response, nil
+}
+
+func (c *PortForwardingClient) UpdatePortForwarding(req PortForwardingRule) (bool, error) {
+	resp, err := c.httpClient.PUT(fmt.Sprintf("network/portforwarding/%d", req.ID), nil, req)
+	if err != nil {
+		return false, err
+	}
+
+	defer resp.Body.Close()
+
+	return resp.StatusCode == 204, nil
+}
+
+func (c *PortForwardingClient) DeletePortForwarding(id int) (bool, error) {
+	resp, err := c.httpClient.DELETE(fmt.Sprintf("network/portforwarding/%d", id), nil)
+	if err != nil {
+		return false, err
+	}
+
+	defer resp.Body.Close()
+
+	return resp.StatusCode == 204, nil
 }
--- modems/sagemcom/port_forwarding_test.go
+++ modems/sagemcom/port_forwarding_test.go
@@ -1,7 +1,9 @@
 package sagemcom
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
 )
 
@@ -15,6 +17,108 @@ func TestGetPortForwarding(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
 	assert.Len(t, resp.PortForwarding.Rules, 2)
+
+	defer client.Logout()
+}
+
+func TestCreatePortForwarding(t *testing.T) {
+	client := NewSagemcomClient(BaseAddress)
+	isLoggedIn, err := client.Login(Password)
+	assert.NoError(t, err)
+	assert.True(t, isLoggedIn)
+
+	port := rand.Intn(65535-1024) + 1024
+
+	rule := PortForwardingRule{}
+	rule.Rule.Enabled = true
+	rule.Rule.ExternalStartPort = port
+	rule.Rule.ExternalEndPort = port
+	rule.Rule.Protocol = "tcp"
+	rule.Rule.LocalStartPort = port
+	rule.Rule.LocalEndPort = port
+	rule.Rule.LocalAddress = "192.168.178.161"
+	rule.Rule.ReadOnly = false
+
+	resp, err := client.PortForwarding.CreatePortForwarding(rule)
+	assert.NoError(t, err, "failed to create port forwarding rule")
+	assert.NotNil(t, resp, "response is nil")
+	assert.Greater(t, resp.Created.ID, 0, "id is not greater than 0")
+	logrus.Infof("Created port forwarding rule: %v", resp)
+
+	client.PortForwarding.DeletePortForwarding(resp.Created.ID)
+	defer client.Logout()
+}
+
+func TestUpdatePortForwarding(t *testing.T) {
+	client := NewSagemcomClient(BaseAddress)
+	isLoggedIn, err := client.Login(Password)
+	assert.NoError(t, err)
+	assert.True(t, isLoggedIn)
+
+	port := rand.Intn(65535-1024) + 1024
+
+	rule := PortForwardingRule{}
+	rule.Rule.Enabled = true
+	rule.Rule.ExternalStartPort = port
+	rule.Rule.ExternalEndPort = port
+	rule.Rule.Protocol = "tcp"
+	rule.Rule.LocalStartPort = port
+	rule.Rule.LocalEndPort = port
+	rule.Rule.LocalAddress = "192.168.178.161"
+	rule.Rule.ReadOnly = false
+
+	resp, err := client.PortForwarding.CreatePortForwarding(rule)
+	assert.NoError(t, err, "failed to create port forwarding rule")
+	assert.NotNil(t, resp, "response is nil")
+	assert.Greater(t, resp.Created.ID, 0, "id is not greater than 0")
+	logrus.Infof("Created port forwarding rule: %v", resp)
+
+	rule.Rule.Enabled = false
+	rule.ID = resp.Created.ID
+	isUpdated, err := client.PortForwarding.UpdatePortForwarding(rule)
+	assert.NoError(t, err, "failed to update port forwarding rule")
+	assert.True(t, isUpdated, "failed to update port forwarding rule")
+
+	rules, err := client.PortForwarding.GetPortForwardings()
+	assert.NoError(t, err, "failed to get port forwarding rules")
+	assert.NotNil(t, rules, "response is nil")
+	for _, r := range rules.PortForwarding.Rules {
+		if r.ID == rule.ID {
+			assert.False(t, r.Rule.Enabled, "rule is not disabled")
+		}
+	}
+
+	//client.PortForwarding.DeletePortForwarding(resp.Created.ID)
+	defer client.Logout()
+}
+
+func TestDeletePortForwarding(t *testing.T) {
+	client := NewSagemcomClient(BaseAddress)
+	isLoggedIn, err := client.Login(Password)
+	assert.NoError(t, err)
+	assert.True(t, isLoggedIn)
+
+	port := rand.Intn(65535-1024) + 1024
+
+	rule := PortForwardingRule{}
+	rule.Rule.Enabled = true
+	rule.Rule.ExternalStartPort = port
+	rule.Rule.ExternalEndPort = port
+	rule.Rule.Protocol = "tcp"
+	rule.Rule.LocalStartPort = port
+	rule.Rule.LocalEndPort = port
+	rule.Rule.LocalAddress = "192.168.178.161"
+	rule.Rule.ReadOnly = false
+
+	resp, err := client.PortForwarding.CreatePortForwarding(rule)
+	assert.NoError(t, err, "failed to create port forwarding rule")
+	assert.NotNil(t, resp, "response is nil")
+	assert.Greater(t, resp.Created.ID, 0, "id is not greater than 0")
+	logrus.Infof("Created port forwarding rule: %v", resp)
+
+	isDeleted, err := client.PortForwarding.DeletePortForwarding(resp.Created.ID)
+	assert.NoError(t, err, "failed to delete port forwarding rule")
+	assert.True(t, isDeleted, "failed to delete port forwarding rule")
 
 	defer client.Logout()
 }
